2afb8a87075069dcc2abee95fd5e660a92f8ad1d
isapsanch <isabelpsanch@hotmail.com>
2023-11-06 17:35:01 +0100

    Añadir Ejercicio 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,15 @@
 // Escribe tu código aquí:
+class Rectangle {
+    constructor(width, height) {
+      this.width = width
+      this.height = height
+    }
+  
+    calculateArea() {
+      return this.width * this.height
+    }
+  }
+  
+  const rectangle = new Rectangle(5, 10)
+  
+  console.log('Area:', rectangle.calculateArea())
