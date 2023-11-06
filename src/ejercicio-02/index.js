0a91bdd3438792b48573f79b0f21669e60aaf6a1
isapsanch <isabelpsanch@hotmail.com>
2023-11-06 17:07:17 +0100

    Añadir Ejercicio 2

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+const car = {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2022
+  }
+  
+  for (let prop in car) {
+    console.log(`${prop}: ${car[prop]}`)
+  }
