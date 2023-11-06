fe8e69fa860166e06165036daef48332e03a6c2d
isapsanch <isabelpsanch@hotmail.com>
2023-11-06 17:01:18 +0100

    Añadir Ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+  }
+  
+  console.log(person.name) // Acceso con notación de puntos
+  console.log(person['age']) // Acceso con notación de corchetes
